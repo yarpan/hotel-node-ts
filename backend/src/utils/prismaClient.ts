1ac79ec6b1bd4082762d0da2a7ec051991c39237
Yaroslav Panchishyn <yarpan@gmail.com>
2026-03-01 19:35:11 +0200

    update_prisma

--- backend/src/utils/prismaClient.ts
+++ backend/src/utils/prismaClient.ts
@@ -1,10 +1,6 @@
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({});
 
 export default prisma;
